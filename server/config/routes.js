7c48376964e32dabbf2c7cdd0bb55e6153746a28
gnoevil <geno.guerrero@gmail.com>
2016-03-24 12:04:15 -0700

    first commit

---
+++ server/config/routes.js
@@ -0,0 +1,71 @@
+console.log("In config routes");
+var user = require('./../controllers/user.js');
+var post = require ('./../controllers/post.js');
+var topic = require('./../controllers/topic.js');
+var comment = require('./../controllers/comment.js');
+
+module.exports = function(app) {
+	app.get('/', function(req, res) {
+		res.render('index');
+	});
+
+	app.get('/user', function(req, res) {
+		user.read(req, res);
+	});
+
+	app.post('/user', function(req, res) {
+		user.create(req, res);
+	})
+
+	app.get('/user/:id', function(req, res) {
+		user.findOne(req, res);
+	})
+
+	app.post('/user/topics/:id', function(req, res) {
+		user.updateTopics(req, res);
+	})
+
+	app.post('/user/posts/:id', function(req, res) {
+		user.updatePosts(req, res);
+	})
+
+	app.post('/user/comments/:id', function(req, res) {
+		user.updateComments(req, res);
+	})
+
+	app.get('/topics', function(req, res) {
+		topic.read(req, res);
+	})
+
+	app.post('/topics', function(req, res) {
+		topic.create(req, res);
+	})
+
+	app.post('/topics/:id', function(req, res) {
+		topic.update(req, res);
+	})
+
+	app.get('/topics/:id', function( req, res) {
+		topic.findOne(req, res);
+	})
+
+	app.post('/posts', function(req, res) {
+		post.create(req, res);
+	})
+
+	app.get('/posts/:id', function(req, res) {
+		post.read(req, res);
+	})
+
+	app.post('/posts/:id', function(req, res) {
+		post.update(req, res);
+	})
+
+	app.post('/comments', function(req, res) {
+		comment.create(req, res);
+	})
+
+	app.get('/comments/:id', function(req, res) {
+		comment.read(req, res);
+	})
+}
